3afda38bd5889e3b2dc5d5f9caecaa91c4ccccb0
Page Asgardius <asgardius@asgardius.company>
2023-08-13 18:54:00 -0700

    test level

---
+++ levels/galaxy.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+var bgsound := AudioStreamPlayer.new()
+
+func _ready():
+	add_child(bgsound)
+	var titlemusic = load("res://music/alien-bulls.mp3")
+	bgsound.stream = titlemusic
+	bgsound.play(0)
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
