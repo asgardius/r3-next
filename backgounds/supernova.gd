cb75d54591d317842b08317436de2a0b86632f40
Page Asgardius <asgardius@asgardius.company>
2023-09-12 09:23:48 -0700

    fix result screen

--- backgounds/supernova.gd
+++ backgounds/supernova.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 var crash := AudioStreamPlayer.new()
+var wait
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(crash)
 	var titlemusic = load("res://sfx/crash.ogg")
 	crash.stream = titlemusic
 	crash.play(0)
+	wait = Time.get_ticks_msec()
 	#pass # Replace with function body.
 
 
@@ -16,7 +18,7 @@ func _process(delta):
 
 func _input(event):
    # Mouse in viewport coordinates.
-	if event is InputEventMouseButton || Input.is_joy_button_pressed(0,JOY_BUTTON_X) || Input.is_joy_button_pressed(0,JOY_BUTTON_Y) || Input.is_joy_button_pressed(0,JOY_BUTTON_A) || Input.is_joy_button_pressed(0,JOY_BUTTON_B) || Input.is_joy_button_pressed(0,JOY_BUTTON_BACK) || Input.is_joy_button_pressed(0,JOY_BUTTON_START) || Input.is_joy_button_pressed(0,JOY_BUTTON_RIGHT_SHOULDER) || Input.is_joy_button_pressed(0,JOY_BUTTON_LEFT_SHOULDER):
+	if (Time.get_ticks_msec() - wait) >= 1000 && (event is InputEventMouseButton || Input.is_joy_button_pressed(0,JOY_BUTTON_X) || Input.is_joy_button_pressed(0,JOY_BUTTON_Y) || Input.is_joy_button_pressed(0,JOY_BUTTON_A) || Input.is_joy_button_pressed(0,JOY_BUTTON_B) || Input.is_joy_button_pressed(0,JOY_BUTTON_BACK) || Input.is_joy_button_pressed(0,JOY_BUTTON_START) || Input.is_joy_button_pressed(0,JOY_BUTTON_RIGHT_SHOULDER) || Input.is_joy_button_pressed(0,JOY_BUTTON_LEFT_SHOULDER)):
 		#print("Mouse Click/Unclick at: ", event.position)
 		_title()
 
